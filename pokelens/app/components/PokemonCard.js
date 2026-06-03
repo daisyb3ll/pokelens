c0b0069071b4b1d10a62d28865cf6421694113b1
daisyb3ll <daisy.fernandez.zr@gmail.com>
2026-06-02 19:02:50 -0700

    editing card layout & page.tsx padding

--- pokelens/app/components/PokemonCard.js
+++ pokelens/app/components/PokemonCard.js
@@ -1,9 +1,19 @@
 export default function PokemonCard({ pokemon }) {
     return (
-        <div>
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-            <h2 className= "text-">{pokemon.name}</h2>
-            <p>{pokemon.types.map(t => t.type.name).join(', ')}</p>
+        <div className ="border border-pink-200 rounded-2xl p-4">
+            <div className="flex-col">
+                <div className="flex justify-center">
+                    <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                </div>
+                <div className= "flex justify-center">
+                        <h2 className="text-2xl">{pokemon.name}</h2>
+                </div>
+                <div className="flex justify-between">
+                    <p>{pokemon.id}</p>
+                    <p>{pokemon.types.map(t => t.type.name).join(' • ')}</p>
+                </div>
+            </div>
+
         </div>
     )
 }
--- pokelens/app/page.js
+++ pokelens/app/page.js
@@ -12,9 +12,9 @@ export default async function Home() {
     <main className= "flex justify-center">
       <div className= "flex-col">
         <div className= "flex justify-center">
-          <h1 className="text-red-400 font-bold text-4xl">pokélens</h1>
+          <h1 className="text-red-400 font-bold text-4xl py-10">pokélens</h1>
         </div>
-      <div className="grid grid-cols-4 gap-10 max-w-4xl mx-auto"> 
+      <div className="grid grid-cols-4 gap-20 max-w-4xl mx-auto"> 
         {pokemon.map((p) => (
           <PokemonCard key={p.id} pokemon={p} />
         ))}
